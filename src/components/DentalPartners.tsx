0733ad498fbfc8a10c99630308a87a6a0fdfe54b
Geoffrey Kithuku <mutembeikithuku12@gmail.com>
2025-04-03 13:59:15 +0300

    fix list of partners

--- src/components/DentalPartners.tsx
+++ src/components/DentalPartners.tsx
@@ -14,32 +14,68 @@ const DentalPartners = () => {
     "Straumann",
     "Coltene",
     "Kerr Dental",
-    "3M Dental",
-    "Dentsply Sirona",
-    "Ivoclar Vivadent",
-    "GC Corporation",
-    "Straumann",
-    "Coltene",
-    "Kerr Dental",
+    "Septodont",
+    "Kavo",
+    "Olident",
+    "Shofu",
+    "Vita",
+    "SureDent",
+    "3A medes",
+    "BMS",
+    "GNI Orthodontics",
+    "LukaDent",
+    "MD Clus",
+    "Milestone",
+    "NHT",
+    "Molars Dental Clinic",
+    "Prevest DenPro",
+    "Renew Medical",
+    "Saeshin",
+    "Taglus",
+    "Warren Dental",
+    "Yucera",
+    "Woodpecker",
+    "AR Instruments",
+    "Ultradent",
+    "Maquira",
+    "Koden",
+    "Acteon",
+    "Waterpik",
+    "ORO",
+    "Bego",
+    "VHF",
+    "Mani",
+    "Amber",
+    "Mespa",
+    "Uzumcu",
+    "Skypro",
+    "Philips",
+    "Intersurgical",
+    "Pinmed",
+    "Medpark",
+    "Dur Dental",
+    "Carestream",
+    "Mindray",
   ];
 
   return (
-    <section className="max-w-6xl mx-auto px-6 py-16" id="partners">
+    <section className="max-w-[1440px] mx-auto px-6 py-16" id="partners">
       <h2 className="text-3xl font-bold text-gray-900 text-center mb-6">
-        Our Partners – Global Leaders in Dental Manufacturing
+        Our Partners – Leaders in Dental & Medical Manufacturing
       </h2>
       <p className="text-lg text-gray-700 text-center mb-8">
-        We collaborate with top-tier dental manufacturers worldwide, ensuring
-        our clients get the best quality products at competitive prices.
+        We collaborate with top-tier dental and medical manufacturers worldwide,
+        ensuring our clients get the best quality products at competitive
+        prices.
       </p>
 
       {/* List of Partners */}
       <div className="bg-white shadow-lg rounded-lg p-8">
-        <ul className="grid md:grid-cols-2 lg:grid-cols-3 gap-4 text-gray-900 text-lg font-medium">
+        <ul className="grid  md:grid-cols-4 lg:grid-cols-5 gap-4 text-gray-900 text-lg font-medium">
           {partners.map((partner, index) => (
             <li
               key={index}
-              className="flex items-center space-x-3 p-4 border-b md:border-none"
+              className="flex items-center space-x-3 p-3 border-b md:border-none"
             >
               <FaCheckCircle className="text-green-500 text-xl" />
               <span>{partner}</span>
